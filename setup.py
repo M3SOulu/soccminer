2b8875c3b0629171aac9f8b993faf6d36182d213
ruyog <muralisridharan.10@gmail.com>
2021-11-07 22:14:24 +0200

    Add files via upload
    initial commit

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+setup(
+    name='soccminer',
+    version='0.0.11a1',
+    packages=['soccminer'],
+    scripts=['bin/main.py'],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Text Processing :: Linguistic',
+      ],
+    platforms='Linux',
+    url='https://github.com/M3SOulu/SoCC-Miner',
+    license='MIT',
+    author='da_enthusiast',
+    author_email='dataapproximation.enthusiast@gmail.com',
+    description='Source Code Comments Miner Tool',
+    install_requires=['pandas', 'lxml', 'numpy', 'python-dateutil', 'six', 'pytz', 'requests', 'certifi', 'charset-normalizer', 'idna', 'urllib3', 'ujson'],
+    zip_safe=False
+)
