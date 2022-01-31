0cef5802d11067691810b7f9a0562fc0d5300748
ruyog <muralisridharan.10@gmail.com>
2022-01-31 08:49:29 +0200

    removing sample evaluation project

--- evaluation_sample/microsoft_appcenter-sampleapp-android/app/appcenter-post-clone.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo "This is an App Center Post-Clone script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
--- evaluation_sample/microsoft_appcenter-sampleapp-android/app/appcenter-pre-build.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-echo "This is an App Center Pre-Build script. For more information on how to use App Center build scripts vist: https://docs.microsoft.com/en-us/appcenter/build/custom/scripts"
